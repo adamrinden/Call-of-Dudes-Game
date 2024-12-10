0b40724c047bfaabcea8cb540135587463cfeaf7
markxsotelo <masotelo05@gmail.com>
2024-12-10 00:16:17 -0800

    Bullet Script
    Created a Bullet script, each bullet it 5 damage, only lasts 3 seconds then is destroyed.

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public float damage;
+    public float lifeTime =3;
+
+    private void Update() {
+        lifeTime -= Time.deltaTime;
+
+        if(lifeTime < 0) 
+            Destroy(gameObject);
+        
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.GetComponent<Target>() != null)
+            other.GetComponent<Target>().health -= damage;
+            
+        Destroy(gameObject);
+    }
+}
