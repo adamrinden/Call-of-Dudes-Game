336788d28d647b28b06706575bd5f4842eee58f7
markxsotelo <masotelo05@gmail.com>
2024-12-10 00:15:21 -0800

    Shooting Script
    Made a shooting script, bullets come out of gun.

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+   [Header("Bullet Variables")]
+   public float bulletSpeed;
+    public float fireRate, bulletDamage;
+   public bool isAuto;
+
+   [Header("Initial Setup")]
+   public Transform bulletSpawnTransform;
+    public GameObject bulletPrefab;
+
+    private float timer;
+
+    private void Update() {
+        if(timer > 0 ) {
+            timer -= Time.deltaTime / fireRate;
+        }
+
+        if (isAuto){
+            if(Input.GetButton("Fire1") && timer <=0) {
+                Shoot();
+            }
+        }
+        else {
+            if(Input.GetButtonDown("Fire1") && timer <=0 ) {
+                Shoot();
+            }
+        }
+    }
+
+    void Shoot() {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnTransform.position, Quaternion.identity, GameObject.FindGameObjectWithTag("WorldObjectHolder").transform);
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawnTransform.forward * bulletSpeed, ForceMode.Impulse);
+        bullet.GetComponent<BulletController>().damage = bulletDamage;
+
+        timer = 1;
+    }
+}
